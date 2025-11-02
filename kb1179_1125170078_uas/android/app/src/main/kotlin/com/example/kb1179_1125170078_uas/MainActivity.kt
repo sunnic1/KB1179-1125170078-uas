3276e13803bc4507911b2a440765c176b9dc0003
Rifqi Arip Sunni M <ariefsunni@gmail.com>
2025-11-02 09:42:25 +0700

    create page utama dan theme

---
+++ kb1179_1125170078_uas/android/app/src/main/kotlin/com/example/kb1179_1125170078_uas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kb1179_1125170078_uas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ kb1179_1125170078_uas/lib/main.dart
@@ -0,0 +1,16 @@
+void main() {
+.....
+}
+// buat file class stateless untuk membuat themes
+//dan title aplikasi sesuai nim anda
+// gunakan shortCut
+
+class MyApp .... {
+....
+....
+
+@override
+Widget build ..... {
+return MaterialApp();
+}
+}
