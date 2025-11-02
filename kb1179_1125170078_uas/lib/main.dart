8401d513ab6a655c745800d5da4fe84d80c038a9
Rifqi Arip Sunni M <ariefsunni@gmail.com>
2025-11-02 09:51:15 +0700

    Update materialapp3

--- kb1179_1125170078_uas/lib/main.dart
+++ kb1179_1125170078_uas/lib/main.dart
@@ -1,16 +1,34 @@
+import 'package:flutter/material.dart';
+
 void main() {
-.....
+  runApp(MyApp());
 }
-// buat file class stateless untuk membuat themes
-//dan title aplikasi sesuai nim anda
-// gunakan shortCut
 
-class MyApp .... {
-....
-....
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-@override
-Widget build ..... {
-return MaterialApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Nim: 1234567, Ketut",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: SplashScreen1(), // memanggil class splash screen 1
+    );
+  }
 }
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Splash Screen 1', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
 }
